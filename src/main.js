503fa7509c467185fdae2cf6a327a17720ee29c0
Rogeroldi <rossanageroldi19ggmail.com>
2019-05-09 09:17:04 -0400

    uniendo todo

--- src/main.js
+++ src/main.js
@@ -1,85 +1,107 @@
 /* Manejo del DOM */
 //declaramos una variable para llamar la data 
 const codePoke = window.POKEMON.pokemon;
-//aqui estamos mostrando todos los pokemon  
-//const root = document.getElementById("root");
-
-//const type = document.getElementById("type");
-
-//console.log(codePoke);
+// Recargamos página
+document.getElementById("recharge").addEventListener("click", () => {
+  location.reload();
+})
 //mostrar toda la data a traves de una variable, tomando un id del body
 let rootBody = document.getElementById("root");
 //aqui llamamos a los pokemon solo por sus nombres en el orden que esta establecido en la data
 let pokepoke = (codePoke) => {
   for (let i = 0; i < codePoke.length; i++) {
     rootBody.innerHTML +=
-      `<div class="col-sm-2">
+    `<div class="card col-sm-2">
       <img src="${codePoke[i].img}" class="card-img-top" alt="Poke">
       <div class="card-body">
-          <div class="col-sm-2">
-        <h5 class="card-title">"Nombre:${codePoke[i].name}"</h5>
-        <h6 class="card-title">"Número:${codePoke[i].num}"</h6>
-        <p class="card-text">"Tipo:${codePoke[i].type}"</p>
-        <a href="#" class="btn-primary">Evolucion</a>
+        <div class="col-sm-2 carta-poke">
+          <h5 class="card-title">${codePoke[i].name}</h5>
+          <h6 class="card-title">Número: ${codePoke[i].num}</h6>
+          <p class="card-text">Tipo: ${codePoke[i].type}</p>
         </div>
-      </div>
-    </div>`
-    //console.log(codePoke[i].name);
+      </div>`
 
   }
 }
 window.onload = pokepoke(codePoke);
 
-//window.selectType = selectType;
-let guardar = document.getElementById("type");
-guardar.addEventListener("change", () => {
+// FILTRADO POR TIPO
+let saveForType = document.getElementById("type");
+saveForType.addEventListener("change", () => {
 
-  let condition = guardar.options[guardar.selectedIndex].value;
+  let condition = saveForType.options[saveForType.selectedIndex].value; // guarda seleccion usiario
   //console.log(condition);
+
+  //COUNTER
+  let typeCounter = window.counterForType(codePoke, condition, "type");
+
   let filter = window.pokedexType(codePoke, condition);
   rootBody.innerHTML = "";
+  document.getElementById("cantPokes").innerHTML =
+    ` <div class= "cantPokes col-12">Existen ${typeCounter} pokemon tipo ${condition}</div> `
 
   filter.forEach(element => {
     rootBody.innerHTML +=
 
-      `<div class="col-sm-2">
-    <img src="${element.img}" class="card-img-top" alt="Poke">
-    <div class="card-body">
-        <div class="col-sm-2">
-      <h5 class="card-title">"Nombre:${element.name}"</h5>
-      <h6 class="card-title">"Número:${element.num}"</h6>
-      <p class="card-text">"Tipo:${element.type}"</p>
-      <a href="#" class="btn-primary">Evolucion</a>
-      </div>
-    </div>
-  </div>`
+    `<div class="col-sm-2">
+      <img src="${element.img}" class="card-img-top" alt="Poke">
+      <div class="card-body">
+          <div class="col-sm-2">
+        <h5 class="card-title">${element.name}</h5>
+        <h6 class="card-title">Número: ${element.num}</h6>
+        <p class="card-text">Tipo: ${element.type}</p>
+        </div>
+      </div>`
   })
 
 });
-window.onload = pokepoke(codePoke);
 
-//window.selectWeaknesses = selectType;
-let guardar = document.getElementById("waknessese");
-guardar.addEventListener("change", () => {
+// FILTRANDO POR DEBILIDADES
+let saveForWeakness = document.getElementById("weaknesses");
+saveForWeakness.addEventListener("change", () => {
 
-  let condition = guardar.options[guardar.selectedIndex].value;
+  let conditionTwo = saveForWeakness.options[saveForWeakness.selectedIndex].value;
   //console.log(condition);
-
-  let filter = window.pokedexWaknessese(codePoke, condition);
+  let filter = window.pokedexWeaknesses(codePoke, conditionTwo);
   rootBody.innerHTML = "";
 
   filter.forEach(element => {
     rootBody.innerHTML +=
-
-      `<div class="col-sm-2">
-       <img src="${element.img}" class="card-img-top" alt="Poke">
-       <div class="card-body">
-           <div class="col-sm-2">
-         <h5 class="card-title">"Nombre:${element.name}"</h5>
-         <h6 class="card-title">"Número:${element.num}"</h6>
-         <p class="card-text">"Tipo:${element.type}"</p>
-         <a href="#" class="btn-primary">Evolucion</a>
-         </div>
-       </div>
-     </div>`
+    `<div class="col-sm-2">
+    <img src="${element.img}" class="card-img-top" alt="Poke">
+    <div class="card-body">
+        <div class="col-sm-2">
+      <h5 class="card-title">${element.name}</h5>
+      <h6 class="card-title">Número: ${element.num}</h6>
+      <p class="card-text">Tipo: ${element.type}</p>
+      </div>
+    </div>`
   })
+
+});
+
+//SORT CORTITO
+
+let ordenAs = document.getElementById("ascdesc");
+
+ordenAs.addEventListener("change", () => {
+  let pokeSort = ordenAs.value;
+
+  let ordenPoke = window.sortCode(codePoke, "name", pokeSort);
+
+  rootBody.innerHTML = "";
+
+  ordenPoke.forEach(element => {
+    rootBody.innerHTML +=
+    `<div class="col-sm-2">
+    <img src="${element.img}" class="card-img-top" alt="Poke">
+    <div class="card-body">
+        <div class="col-sm-2">
+      <h5 class="card-title">${element.name}</h5>
+      <h6 class="card-title">Número: ${element.num}</h6>
+      <p class="card-text">Tipo: ${element.type}</p>
+      </div>
+    </div>`
+  })
+
+}) 
